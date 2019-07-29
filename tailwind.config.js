64ba214ecca29e4b5b5b24087071735373e1a7e8
Luke Joseph <illuzual@gmail.com>
2019-07-29 10:50:16 +0200

    minified images

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,8 +2,11 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        // gray:{
-        // }
+        action:{
+          100: '#D3EFD2',
+          500: '#B3EFB1',
+          800: '#A2D8A0',
+        }
       },
     }
   },
