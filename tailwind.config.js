c5b0022513d8683ba3f75b18ce15cc7e4998e403
Luke Joseph <luke@trackwire.io>
2020-08-09 17:57:36 +0200

    update the links and replace urbaniks for scary maze

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,7 @@ module.exports = {
     extend: {
       colors: {
         action:{
-          100: '#D3EFD2',
+          100: '#e6fffa',
           500: '#B3EFB1',
           800: '#A2D8A0',
         }
